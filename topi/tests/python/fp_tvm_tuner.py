2f0e7cc34e5e1d9cb6d5bf81d2ef0eea3d837e1c
Tharindu Rusira <tharindurusira@gmail.com>
2018-11-04 19:24:39 -0800

    AutoTVM based tuner for convolution (forward pass)

--- topi/tests/python/fp_tvm_tuner.py
+++ topi/tests/python/fp_tvm_tuner.py
@@ -22,7 +22,7 @@ _3x3_layers ={
     'vgg2_1':[64,128,64,112,112,3,1,1],'vgg3_1':[64,256,128,56,56,3,1,1], 'vgg3_2':[64,256,256,56,56,3,1,1], 'vgg4_1':[64,512,256,28,28,3,1,1],
     'vgg4_2':[64,512,512,28,28,3,1,1],'vgg5_2':[64,512,512,14,14,3,1,1],'alex3':[128,384,192,13,13,3,1,1],
     'alex4':[128,256,384,13,13,3,1,1],'alex5':[128,256,256,13,13,3,1,1],
-    'overfeat3':[64,512,256,12,12,3,1,1], 'overfeat4':[64,1024,512,12,12,3,1,1], 'overfeat5':[64,1024,1024,12,12,3,1,1], 'resnet1':[1,64,64,56,56,3,1,1]}
+    'overfeat3':[64,512,256,12,12,3,1,1], 'overfeat4':[64,1024,512,12,12,3,1,1], 'overfeat5':[64,1024,1024,12,12,3,1,1], 'resnet1':[1,64,64,56,56,3,1,1],  'test':[2,32,32,12,12,3,1,1]}
 
 # The sizes of inputs and filters
 batch = _3x3_layers[layer][0]
@@ -45,11 +45,7 @@ padding = pad_height
 output_width = ((input_width + 2 * pad_width - kernel_width) // stride_width) + 1
 output_height = ((input_height + 2 * pad_height - kernel_height) // stride_height) + 1
 
-'''
-adims = (batch, math.ceil(in_channel/vlen) , input_height + 2*pad_height, input_width+ 2*pad_width, vlen)
-wdims = (math.ceil(out_channel/vlen), math.ceil(in_channel/vlen), kernel_height,kernel_width, vlen, vlen)
-bdims = (batch, math.ceil(out_channel/vlen), output_height, output_width, vlen)
-'''
+
 h_list = []
 w_list = []
 c_list = []
@@ -58,88 +54,16 @@ c_list = []
 def forward(A, W, B):
     for n in range(0,batch):
         for ko in range(0,math.ceil(out_channel/vlen)):
-            for h in range(0, output_height):
-                for w in range(0,output_width):
-                    for co in range(0, math.ceil(in_channel/vlen)):
+            for p in range(0, output_height):
+                for q in range(0,output_width):
+                    for co in range(0, A.shape[1]):
                         for r in range(0,kernel_height):
                             for s in range(0,kernel_width):
-                                for ci in range(0,vlen):
+                                for ci in range(0, A.shape[4]):
                                     for ki in range(0,vlen): 
-                                        B[ko][co][h+r][w+s][ki][ci] += A[n][ko][h][w][ki]*W[n][co][r][s][ci]
+                                        B[n][ko][p][q][ki] += A[n][co][p+r][q+s][ci]*W[ko][co][r][s][ci][ki]
 
     return B
-
-
-@autotvm.template
-def convolution((dtype)):
-  cfg = autotvm.get_config()
- 
-  for i in range(2,in_channel):
-    if in_channel%i == 0:
-      c_list.append(i)
- 
-  cfg.define_knob("tile_c", c_list)
-
-  co_dim, ci_dim = in_channel/cfg['tile_c'].val, cfg['tile_c'].val
-
-  adims = (batch, co_dim , input_height + 2*pad_height, input_width+ 2*pad_width, ci_dim)
-  wdims = (math.ceil(out_channel/vlen), co_dim, kernel_height,kernel_width, ci_dim, vlen)
-  bdims = (batch, math.ceil(out_channel/vlen), output_height, output_width, vlen)
-
-
-  A = tvm.placeholder(adims, dtype=dtype, name='A')
-  W = tvm.placeholder(wdims, dtype=dtype, name='W')
-
-  #rco = tvm.reduce_axis((0, math.ceil(in_channel/vlen)), name='rco')
-  #rci =  tvm.reduce_axis((0, vlen), name="rci")
-  rco = tvm.reduce_axis((0,co_dim), name='rco')
-  rci =  tvm.reduce_axis((0, ci_dim), name="rci")
- 
-  ry = tvm.reduce_axis((0, kernel_height), name='ry')
-  rx = tvm.reduce_axis((0, kernel_width), name='rx')
-
-  B = tvm.compute(bdims, lambda n, ko, h, w, ki: tvm.sum(A[n, rco, h + ry, w + rx, rci] * W[ko, rco, ry, rx, rci, ki], axis=[rco, rci ,ry, rx]), name='B')
-  func = B
-  s = tvm.create_schedule(func.op)
-
-  n, ko, h,w, ki  = s[func].op.axis
-  rco, rci, ry,rx = s[func].op.reduce_axis
-
- 
-  for i in range(2,input_height):
-    if input_height%i == 0:
-      h_list.append(i)
-  for j in range(2,input_width):
-    if input_width%j == 0:
-      w_list.append(j)
-
-  cfg.define_knob("tile_h", h_list)
-  cfg.define_knob("tile_w", w_list)
-
-  ho, hi = s[func].split(h, cfg['tile_h'].val)
-  wo, wi = s[func].split(w, cfg['tile_w'].val)
-
-  nko = s[func].fuse(n,ko)
-  
-  #TODO: tune loop orders
-  #o1 = (nko, ho, wo, rco, hi, wi, rx, ry, rci, ki)
-  #o2 = (nko, wo, ho, rco, wi, hi, rx, ry, rci, ki)
-  #cfg.define_knob("orders", [o1, o2])
-  #print(cfg['orders'].val)
-  #s[func].reorder(cfg['orders'].val) 
-
-  s[func].reorder(nko, ho, wo, rco, hi, wi, rx, ry, rci, ki)
-
-  #fixed 
-  s[func].vectorize(ki)
-  s[func].unroll(rci)
-  s[func].parallel(nko)
-  #with tvm.build_config(data_alignment=64):
-  #print(tvm.lower(s, [A, W, B], simple_mode=True))
-  
-  return s,  [A, W, B]
-
-
 def test():
   A = tvm.placeholder(adims, name='A')
   W = tvm.placeholder(wdims, name='W')
@@ -168,9 +92,7 @@ def test():
   s[func].vectorize(ki)
   s[func].parallel(nko)
   s[func].unroll(rci)
-
   return s, A, W, B
-
 def compile_and_run(s, A, W, B):
 
   with tvm.build_config(data_alignment=64):
@@ -197,9 +119,89 @@ def compile_and_run(s, A, W, B):
         print("GFLOPS  : {0:.3f} ".format( gflops))
   return 0, gflops
 
-def driver():
+@autotvm.template
+def convolution():
+
+  cfg = autotvm.get_config()
+
+  for i in range(2,in_channel):
+    if in_channel%i == 0:
+      c_list.append(i)
+
+  # C tile factor
+  cfg.define_knob("tile_c", c_list)
+    
+  #rco = tvm.reduce_axis((0, math.ceil(in_channel/vlen)), name='rco')
+  #rci =  tvm.reduce_axis((0, vlen), name="rci")
+
+  co_dim, ci_dim = math.ceil(in_channel/cfg['tile_c'].val), cfg['tile_c'].val 
   
-    task = autotvm.task.create(convolution, args=('float32'), target='llvm')
+  rco = tvm.reduce_axis((0,co_dim), name='rco')
+  rci =  tvm.reduce_axis((0, ci_dim), name="rci")
+ 
+  ry = tvm.reduce_axis((0, kernel_height), name='ry')
+  rx = tvm.reduce_axis((0, kernel_width), name='rx')
+
+  #adims = (batch, math.ceil(in_channel/vlen) , input_height + 2*pad_height, input_width+ 2*pad_width, vlen)
+  #wdims = (math.ceil(out_channel/vlen), math.ceil(in_channel/vlen), kernel_height,kernel_width, vlen, vlen)
+  #bdims = (batch, math.ceil(out_channel/vlen), output_height, output_width, vlen)
+
+  adims = (batch, co_dim , input_height + 2*pad_height, input_width+ 2*pad_width, ci_dim)
+  wdims = (math.ceil(out_channel/vlen), co_dim, kernel_height,kernel_width, ci_dim, vlen)
+  bdims = (batch, math.ceil(out_channel/vlen), output_height, output_width, vlen)
+
+  A = tvm.placeholder(adims, name='A', dtype='float32')
+  W = tvm.placeholder(wdims, name='W', dtype='float32')
+
+
+  B = tvm.compute(bdims, lambda n, ko, h, w, ki: tvm.sum(A[n, rco, h + ry, w + rx, rci] * W[ko, rco, ry, rx, rci, ki], axis=[rco, rci ,ry, rx]), name='B')
+  func = B
+  s = tvm.create_schedule(func.op)
+
+  n, ko, h,w, ki  = s[func].op.axis
+  rco, rci, ry,rx = s[func].op.reduce_axis
+ 
+  for i in range(2,input_height):
+    if input_height%i == 0:
+      h_list.append(i)
+  for j in range(2,input_width):
+    if input_width%j == 0:
+      w_list.append(j)
+
+  #Tile H and W  
+  cfg.define_knob("tile_h", h_list)
+  cfg.define_knob("tile_w", w_list)
+
+  ho, hi = s[func].split(h, cfg['tile_h'].val)
+  wo, wi = s[func].split(w, cfg['tile_w'].val)
+  
+  cfg.define_knob('nk_fuse', ['fuse', 'no-fuse'])
+
+  if cfg['nk_fuse'].val == 'fuse':
+    nko = s[func].fuse(n,ko)
+    s[func].reorder(nko, ho, wo, rco, hi, rx, ry, wi, rci, ki)
+    s[func].parallel(nko)
+
+  else:
+    s[func].reorder(n, ko, ho, wo, rco, hi, rx, ry, wi, rci, ki)
+    s[func].parallel(n)
+
+  cfg.define_knob('ki_vectorize', ['vectorize', 'no-vectorize'])
+  if cfg['ki_vectorize'].val == 'vectorize':
+      s[func].vectorize(ki)
+  
+  cfg.define_knob('unroll_loop', ['rci', 'wi'])
+  if cfg['unroll_loop'].val == 'rci':
+    s[func].unroll(rci)
+  else:
+    s[func].unroll(wi)
+  
+  #TODO: tune loop orders
+
+  return s,  [A, W, B]
+
+def driver():
+    task = autotvm.task.create(convolution, args=(), target='llvm')
     print(task.config_space)
 
     logging.getLogger('autotvm').setLevel(logging.DEBUG)
@@ -207,25 +209,33 @@ def driver():
 
     measure_option = autotvm.measure_option(builder=autotvm.LocalBuilder(), runner=autotvm.LocalRunner(number=5, timeout=100))
     tuner = autotvm.tuner.RandomTuner(task)
-    tuner.tune(n_trial=100, measure_option=measure_option, callbacks=[autotvm.callback.log_to_file("tune.log")])
+    tuner.tune(n_trial=10, measure_option=measure_option, callbacks=[autotvm.callback.log_to_file("tune.log")])
 
-'''
-#INCOMPLETE
 def verify_history_best():
-    with autotvm.apply_verify_history_best('tune.log'):
-        with tvm.target.create('llvm'):
-            s, bufs = convolution(adim, wdim, bdim, 'float32')
-            func = tvm.build(s, bufs)
+    with autotvm.apply_history_best('tune.log'):
+        with tvm.target.create('llvm -mtriple=x86_64 -mcpu=skylake-avx512 -mattr=+skx,+fma,+fma4,+avx512ifma,+avx512f,+avx512cd,+avx512bw,+avx512vl,+avx512dq'):
+            s, bufs = convolution()
+            print(bufs)
+            func = tvm.build(s, bufs[:3])
 
-    a_np = np.random.uniform(size=adims).astype(np.float32)
-    w_np = np.random.uniform(size=wdims).astype(np.float32)
+    adims, wdims, bdims = bufs[0].shape, bufs[1].shape, bufs[2].shape
+    #print(adims, type(adims), type(adims[0]))
+    Adims, Wdims, Bdims = [], [], []
+    for i in adims:
+        Adims.append(int(i))
+    for j in wdims:
+        Wdims.append(int(j))
+    for k in bdims:
+       Bdims.append(int(k))
+ 
+    a1 = np.random.uniform(size=tuple(Adims)).astype(np.float32)
+    w1 = np.random.uniform(size=tuple(Wdims)).astype(np.float32)
+    b_ref = forward(a1, w1, np.zeros(tuple(Bdims), dtype=np.float32))
 
-    b_np = forward(a_np, w_np, np.zeros(bdims))
-    b_tvm = tvm.nd.empty(bdims)
-    func(tvm.nd.array(a_np), tvm.nd.array(w_np), )
+    b1 = tvm.nd.empty(b_ref.shape)
+    func(tvm.nd.array(a1), tvm.nd.array(w1), b1)
 
-    tvm.testing.assert_allclose()
-'''
+    np.testing.assert_allclose(b1.asnumpy(), b_ref, rtol=1e-5)
 
 if __name__ == "__main__":
     if args.debug:
@@ -234,5 +244,4 @@ if __name__ == "__main__":
         compile_and_run(s, A, W, B)
     else:
         driver()
-        #verify_history_best()
-
+        verify_history_best()
